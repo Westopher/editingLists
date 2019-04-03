c7b0ea5ca193373cfe66acb99eadb9331d4302d6
West Kraemer <kraemerwest@gmail.com>
2019-04-03 12:59:13 -0400

    add emojis to populate rows

--- editingAndReOrdering/ViewController.swift
+++ editingAndReOrdering/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    let emojis = allEmojis
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,11 +23,13 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return emojis.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
     }
     
     
